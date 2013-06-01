132718f05b800099e745a24c76da63afe9996ba0
aiweiyangxiao <402976935@qq.com>
2013-06-01 18:25:26 +0800

    06011825

--- src/com/example/voter/Launch.java
+++ src/com/example/voter/Launch.java
@@ -1,16 +1,10 @@
 package com.example.voter;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -39,24 +33,31 @@ public class Launch extends Activity{
 		multioption=(Button)findViewById(R.id.multi);
 		radioGroup=(RadioGroup)findViewById(R.id.sel2);
 		radioGroup.setOnCheckedChangeListener(listen);
-		 @SuppressWarnings("unused")
-		OnCheckedChangeListener  listen=new OnCheckedChangeListener() {
-	         @Override
-			public void onCheckedChanged(CompoundButton buttonView,
-					boolean isChecked) {
-	        	 switch (radioGroup.getCheckedRadioButtonId()) {
-	        	             case R.id.pub:
-	        	                 break;
-	        	             case R.id.self:
-	        	            	 {
-	        	            	 add_password();
-	        	                 break;
-	        	             }				
-			}
-	         }
-		 };
-		passwordButton=(RadioButton)findViewById(R.id.self);
-		nonpasswordButton=(RadioButton)findViewById(R.id.pub);
+		
+		nonpasswordButton=(RadioButton)findViewById(R.id.self);
+		passwordButton=(RadioButton)findViewById(R.id.pub);
+		passwordButton.setOnClickListener(new View.OnClickListener() {       
+		       public void onClick(View v) {
+	    	   intent1.setClass(Launch.this,Password.class);
+				startActivity(intent1);}});
+		//OnCheckedChangeListener  listen=new OnCheckedChangeListener() {
+	         //@Override
+			//public void onCheckedChanged(CompoundButton buttonView,
+				//	boolean isChecked) {
+	        	 //switch (radioGroup.getCheckedRadioButtonId()) {
+	        	   //          case R.id.pub:
+	        	     //            break;
+	        	       //      case R.id.self:
+	        	         //   	 {
+	        	            		 
+	        	    		//				}
+	        	    			//    });
+	        	                 //break;
+	        	             //}				
+			//}
+	         //}
+		 //};
+		
 		formeroptionEditText=(EditText)findViewById(R.id.de_vote_option_1);
 		formsetLayout=(LinearLayout)findViewById(R.id.newvotes_layout);
 		additionButton=(Button)findViewById(R.id.add_option);
@@ -70,38 +71,11 @@ public class Launch extends Activity{
 
 
 	public  void add_option(int num){
-		new_optionEditText=(EditText) new EditText(this);//ERROR
+		new_optionEditText=(EditText) new EditText(this);
 		new_optionEditText.setHint("请输入第" + num + "个选项");
 		formsetLayout.addView(new_optionEditText);
 		
 	}
-	public void add_password(){
-		//intent1.setClass(Launch.this,Password.class);
-		//startActivity(intent1);
-		LayoutInflater layoutInflater = LayoutInflater.from(this); 
-        View myLoginView = layoutInflater.inflate(R.layout.password, null); 
-         
-        Dialog alertDialog = new AlertDialog.Builder(this). 
-                setTitle("密码框"). 
-                setIcon(R.drawable.ic_launcher). 
-                setView(myLoginView). 
-                setPositiveButton("确定", new DialogInterface.OnClickListener() { 
- 
-                    @Override 
-                    public void onClick(DialogInterface dialog, int which) { 
-                        // TODO Auto-generated method stub  
-                    } 
-                }). 
-                setNegativeButton("取消", new DialogInterface.OnClickListener() { 
- 
-                  //  @Override 
-                    public void onClick(DialogInterface dialog, int which) { 
-                        // TODO Auto-generated method stub  
-                    } 
-                }). 
-                create(); 
-        alertDialog.show(); 
-	
-}
+
 		 }
 	
