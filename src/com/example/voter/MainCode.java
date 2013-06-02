d79f7d2cc6df7aca8e8887155afc335ccb1db5ce
aiweiyangxiao <402976935@qq.com>
2013-06-02 12:03:31 +0800

    fuck

--- src/com/example/voter/MainCode.java
+++ src/com/example/voter/MainCode.java
@@ -10,38 +10,30 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainCode extends Activity{
-	LinearLayout layout;
-	TextView show;
 	Button launch;
 	Button join;
 	Button help;
+	TextView show;
+	LinearLayout layout;
 	Intent intent =new Intent();
 	@Override
 	public void onCreate(Bundle saveInstanceState){
 		Bundle savedInstanceState = null;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		layout = (LinearLayout)findViewById(R.id.welcome);
-		layout.setOrientation(LinearLayout.VERTICAL);
-		show = new TextView(this);
-		layout.addView(show);
-		launch = (Button)findViewById(R.id.LaunchButton);
-		join = (Button)findViewById(R.id.JoinButton);
-		help = (Button)findViewById(R.id.HelpButton);	
-		
+		launch=(Button)findViewById(R.id.LaunchButton);
+		join=(Button)findViewById(R.id.JoinButton);
+		help=(Button)findViewById(R.id.HelpButton);
+		show=new TextView(this);
 		//launch.setOnClickListener((OnClickListener) this);
+		layout =(LinearLayout)findViewById(R.id.welcome);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		layout.addView(show);		
 		launch.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				intent.setClass(MainCode.this, Launch.class);
-				startActivity(intent);
-			}
-		});
-		join.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				intent.setClass(MainCode.this, Join.class);
-				startActivity(intent);
-			}
-		});
+			       public void onClick(View v) {
+			    	   intent.setClass(MainCode.this,Launch.class);
+						startActivity(intent);
+						}
+			    });
 }
 }
