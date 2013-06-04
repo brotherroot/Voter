c344cff27541d4a7f9374e1670af809b8b685e90
brotherroot <brotherroot1993@gmail.com>
2013-06-05 01:31:47 +0800

    精简Launch 修正了添加选项时标号的错误

--- src/com/example/voter/Launch.java
+++ src/com/example/voter/Launch.java
@@ -1,10 +1,7 @@
 package com.example.voter;
 
-import com.example.voter.Launch;
 import com.example.voter.R;
-import com.example.voter.Show;
 
-import android.R.string;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,102 +11,45 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-public class Launch extends Activity{
-//	private static final android.widget.RadioGroup.OnCheckedChangeListener listen = null;
-	int opnum=2;
-	string password;
+public class Launch extends Activity {
+	int opnum = 2;
 	RelativeLayout whole;
-	Button singleoption;
-	Button multioption;
-	LinearLayout formsetLayout;
-//	Button add_optionButton, savevoteButton;
-	Button additionButton;
-	Button launchButton;
-	Intent intent1 =new Intent();
-    EditText new_optionEditText;
-//	EditText formeroptionEditText;
-//	EditText passwordmarkEditText;
-//	RadioButton passwordButton;
-//	RadioButton nonpasswordButton;
-//	RadioGroup radioGroup;
-	public void onCreate(Bundle saveInstanceState){
+	LinearLayout layout_options;
+	Button button_add;
+	Button button_launch;
+	
+	public void onCreate(Bundle saveInstanceState) {
 		Bundle savedInstanceState = null;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.launch);
 		
 		whole = (RelativeLayout)findViewById(R.id.whole);	
-		//passwordButton=(RadioButton)findViewById(R.id.pub);
-//		passwordButton.setOnClickListener(new View.OnClickListener() {       
-//		       public void onClick(View v) {
-//	    	   add_password(); 
-//	    	   }});
-		formsetLayout = (LinearLayout)findViewById(R.id.option);
-		additionButton = (Button)findViewById(R.id.add_option);
-		additionButton.setOnClickListener(new View.OnClickListener() {
+		
+		layout_options = (LinearLayout)findViewById(R.id.option);
+		button_add = (Button)findViewById(R.id.add_option);
+		button_add.setOnClickListener(new View.OnClickListener() {
+			@Override
 			 public void onClick(View v) {
-				 add_option(opnum);
-				 opnum++;
+				 ++opnum;
+				 EditText new_option = new EditText(Launch.this);
+				 new_option.setHint("请输入第" + opnum + "个选项");
+				 layout_options.addView(new_option);
 			 }
 		});
-		launchButton=(Button)findViewById(R.id.launch);
-		launchButton.setOnClickListener(new View.OnClickListener() {
-			
+		
+		button_launch = (Button)findViewById(R.id.launch);
+		button_launch.setOnClickListener(new View.OnClickListener() {	
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				VoteClass new_vote = new VoteClass(whole);
-		
+				
 				Bundle bundle =new Bundle();
 				bundle.putSerializable("data", new_vote);
 				Intent intent = new Intent(Launch.this, Show.class);
 				intent.putExtras(bundle);
 				startActivity(intent);
-				
-//				Intent intent = new Intent(Launch.this, Show.class);
-//				/* 通过Bundle对象存储需要传递的数据 */
-//				Bundle bundle = new Bundle();
-//				/* 字符、字符串、布尔、字节数组、浮点数等等，都可以传 */
-//				bundle.putString("title", "12345");
-//				bundle.putString("description", new_vote.getDescription());
-//				bundle.putString("addid",new_vote.getLauncher());
-//				/* 把bundle对象assign给Intent */
-//				intent.putExtras(bundle);
-//				startActivityForResult(intent, 0);
 			}
 		});
 	}
-
-
-	public  void add_option(int num){
-		new_optionEditText=(EditText) new EditText(this);
-		new_optionEditText.setHint("请输入第" + num + "个选项");
-		formsetLayout.addView(new_optionEditText);
-		
-	}
-	//public void add_password(){
-	//	LayoutInflater layoutInflater = LayoutInflater.from(this); 
-       // View myLoginView = layoutInflater.inflate(R.layout.password, null); 
-         
-       // Dialog alertDialog = new AlertDialog.Builder(this). 
-        //        setTitle("密码框"). 
-         //     setIcon(R.drawable.ic_launcher). 
-           //     setView(myLoginView). 
-            //    setPositiveButton("确定", new DialogInterface.OnClickListener() { 
- 
-              //      @Override 
-               //     public void onClick(DialogInterface dialog, int which) { 
-                        // TODO Auto-generated method stub  
-             //   } 
-             //  }). 
-              //  setNegativeButton("取消", new DialogInterface.OnClickListener() { 
- 
-              //    @Override 
-               //   public void onClick(DialogInterface dialog, int which) { 
-                        // TODO Auto-generated method stub  
-              //   } 
-              //  }). 
-           //     create(); 
-      //  alertDialog.show(); 
-	//}
-		 }
+}
 	
