996d29f4cced168c653e40798c0a5c53b53f9571
brotherroot <brotherroot1993@gmail.com>
2013-06-05 00:31:57 +0800

    完成选项选择的功能 vote的布局有问题

--- src/com/example/voter/Vote.java
+++ src/com/example/voter/Vote.java
@@ -2,10 +2,21 @@ package com.example.voter;
 
 import java.util.ArrayList;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 
@@ -15,9 +26,10 @@ public class Vote extends Activity {
 	TextView text_launcher;
 	TextView text_topic;
 	TextView text_description;
-	TextView text_type;
-	LinearLayout layout_options;
+	RadioGroup group_options;
+	Button button_vote;
 	
+	@SuppressLint("NewApi")
 	public void onCreate(Bundle saveInstanceState) {
 		Bundle savedInstanceState = null;
 		super.onCreate(savedInstanceState);
@@ -38,18 +50,33 @@ public class Vote extends Activity {
 		text_description = (TextView)findViewById(R.id.description);
 		text_description.setText(vote_status.getDescription().toString());
 		
-//		text_type = (TextView)findViewById(R.id.type);
-//		text_type.setText(getResources().getString(vote_status.isSingle() ? R.string.single : R.string.multiply));
-		
-		layout_options = (LinearLayout)findViewById(R.id.options);
+		group_options = (RadioGroup)findViewById(R.id.options);
 		ArrayList<OptionClass> options = vote_status.getOptions();
-		TextView new_option = null;
-		for (OptionClass option:options) {
-			new_option = (TextView)new TextView(this);
-			new_option.setText(option.getOption());
-			new_option.setBottom(R.dimen.activity_horizontal_margin);
-			layout_options.addView(new_option);
+		RadioButton new_button = null;
+		TextView new_weight = null;
+		LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		param.rightMargin = -10; 
+		for (OptionClass option:options) {	
+			new_button = new RadioButton(this);
+			new_button.setText(option.getOption());
+			group_options.addView(new_button);
+			
+			new_weight = new TextView(this);
+			new_weight.setText(option.getWeight().toString());
+			new_weight.setLayoutParams(param);
+			
+			group_options.addView(new_weight);
 		}
-
+		group_options.check(1);
+		
+		
+		button_vote = (Button)findViewById(R.id.buttom_vote);
+		button_vote.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Log.d("Vote", "id =  " + group_options.getCheckedRadioButtonId());
+				String select = "[" + (group_options.getCheckedRadioButtonId() -1) + "]";
+				// TODO post
+			}
+		});
 	}
 }
