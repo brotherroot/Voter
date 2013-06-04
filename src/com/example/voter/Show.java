49e4a37330c750a2e086e2ba98430302c36a3091
aiweiyangxiao <402976935@qq.com>
2013-06-04 19:54:03 +0800

    fix showvote

--- src/com/example/voter/Show.java
+++ src/com/example/voter/Show.java
@@ -1,21 +1,18 @@
 package com.example.voter;
 
-import com.example.voter.Launch;
 import com.example.voter.R;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class Show extends Activity{
 	
 	private TextView titleTextView;
 	private TextView descriptionTextView;
-	private TextView addidTextView;
+	private TextView LuncherTextView;
+	private TextView TimeTextView;
 
 	public void onCreate(Bundle saveInstanceState){
 		Bundle savedInstanceState = null;
@@ -33,10 +30,13 @@ public class Show extends Activity{
 //		
 		titleTextView = (TextView)findViewById(R.id.title);
 		descriptionTextView = (TextView)findViewById(R.id.describe);
-		addidTextView = (TextView)findViewById(R.id.launcher);
-
+		LuncherTextView = (TextView)findViewById(R.id.launcher);
+		TimeTextView=(TextView)findViewById(R.id.timerequest);
 		titleTextView.setText("您的话题是："+new_vote.getTopic());
-		descriptionTextView.setText("描述:\n"+new_vote.getDescription());
+		descriptionTextView.setText("描述:\n  "+new_vote.getDescription());
+		LuncherTextView.setText("发起者："+new_vote.getLauncher());
+		TimeTextView.setText("时限："+new_vote.getLeftTime());
+		
 
 	}
 	
